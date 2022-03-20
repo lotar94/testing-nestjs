756ea724a45f05ae9c34421f717bc3530a54a38e
Lotar Medina <lantmedina@MacBook-Pro-de-Lotar-Antonio-Medina-Contreras.local>
2022-03-20 18:27:55 -0300

    nest-config module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,66 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService, private jwt: JwtService) {}
+
+  async signup(dto: AuthDto) {
+    //generate the password hash
+    const hash = await argon.hash(dto.password);
+    //save the new user
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      //return the saved user
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
+  }
+
+  async signin(dto: AuthDto) {
+    //find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    //if user does not exit  throw exception
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+
+    //compare password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    //if password incorrect throw exception
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
+
+    //send back the user
+    delete user.hash;
+    return user;
+  }
+
+  async signToken(userId: number, email: string) {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    //TODO:
+    return this.jwt.signAsync(payload, { expiresIn: '15m', secret: '' });
+  }
+}
